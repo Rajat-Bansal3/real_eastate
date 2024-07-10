9fa8765c4b642118e77126f7b419c5782f0a3442
rajat bansal <rajatbansal28082003@gmail.com>
2024-07-10 08:44:39 +0530

    deploying

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import listingRouter from "./routes/listing.routes.js";
-import path from 'path'
+import path from "path";
 
 dotenv.config();
 
@@ -17,15 +17,21 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("connect to mongo");
 });
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/my-project/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "my-project/dist/index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
