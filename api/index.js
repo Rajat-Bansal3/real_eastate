fd45e46bf160e5fcfe05d19add8204698a88c505
rajat bansal <rajatbansal28082003@gmail.com>
2024-07-10 08:58:38 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -23,10 +23,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/my-project/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "my-project/dist/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 app.use((err, req, res, next) => {
